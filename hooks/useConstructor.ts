b4b611c80522665bae466a106c97131ccb9692dd
Nguyen Ha Hiep <hiephanguyen4401@gmail.com>
2022-06-29 00:06:29 +0700

    test

---
+++ hooks/useConstructor.ts
@@ -0,0 +1,12 @@
+import { useRef } from "react";
+
+type ConstructorCallback = () => void;
+
+function useConstructor(callback:ConstructorCallback):void{
+    const isRun = useRef(false)
+
+    if(!isRun.current){
+        callback();
+        isRun.current = true
+    }
+}
